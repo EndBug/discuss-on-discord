832cc6d1095c73b1420572ddb56ac34360d7f5ff
Federico Grandi <fgrandi30@gmail.com>
2023-05-08 23:03:32 +0200

    fix: optimize by not logging in to Discord when unnecessary

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,39 @@ import {createThread, initClient} from './discord';
 import {commentOnIssue} from './github';
 import {getInputs, setOutput} from './io';
 import {getIssueCommentLink, getThreadLink} from './utils';
+import {Client} from 'discord.js';
 
 (async () => {
   core.startGroup('Checking inputs...');
   const inputs = await getInputs();
   core.endGroup();
 
-  return initClient(inputs, async client => {
-    try {
-      let threadLink = inputs.existing_discord_thread;
+  const commentAndExit = async (threadLink: string, client?: Client) => {
+    const commentID = await commentOnIssue(inputs, threadLink);
 
-      if (!threadLink) {
+    if (typeof commentID === 'number') {
+      const commentLink = getIssueCommentLink(inputs.issue, commentID);
+
+      setOutput('comment_id', commentID);
+      setOutput('comment_link', commentLink);
+    } else if (commentID !== null)
+      throw new Error('Could not get a valid comment ID: ' + commentID);
+
+    core.info('Done! Exiting...');
+    client?.destroy();
+    // eslint-disable-next-line no-process-exit
+    process.exit();
+  };
+
+  let threadLink = inputs.existing_discord_thread;
+  if (threadLink) {
+    core.info('Using existing thread...');
+    setOutput('thread_id', threadLink.split('/').pop()!);
+    setOutput('thread_link', threadLink);
+    return commentAndExit(threadLink);
+  } else
+    return initClient(inputs, async client => {
+      try {
         core.info('Creating a new thread...');
         const thread = await createThread(client, inputs);
 
@@ -23,34 +45,17 @@ import {getIssueCommentLink, getThreadLink} from './utils';
 
         threadLink = getThreadLink(thread);
         setOutput('thread_id', thread.id);
-      } else {
-        core.info('Using existing thread...');
-        setOutput('thread_id', threadLink.split('/').pop()!);
+        setOutput('thread_link', threadLink);
+
+        return commentAndExit(threadLink, client);
+      } catch (e) {
+        core.endGroup();
+        core.setFailed(e instanceof Error ? e : '' + e);
+        client.destroy();
+        // eslint-disable-next-line no-process-exit
+        process.exit();
       }
-      setOutput('thread_link', threadLink);
-
-      const commentID = await commentOnIssue(inputs, threadLink);
-
-      if (typeof commentID === 'number') {
-        const commentLink = getIssueCommentLink(inputs.issue, commentID);
-
-        setOutput('comment_id', commentID);
-        setOutput('comment_link', commentLink);
-      } else if (commentID !== null)
-        throw new Error('Could not get a valid comment ID: ' + commentID);
-
-      core.info('Done! Exiting...');
-      client.destroy();
-      // eslint-disable-next-line no-process-exit
-      process.exit();
-    } catch (e) {
-      core.endGroup();
-      core.setFailed(e instanceof Error ? e : '' + e);
-      client.destroy();
-      // eslint-disable-next-line no-process-exit
-      process.exit();
-    }
-  });
+    });
 })().catch(e => {
   core.endGroup();
   core.setFailed(e);
